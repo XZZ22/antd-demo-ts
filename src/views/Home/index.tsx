6dc0b20be92fb4cae8827d469cd22432853177ba
许真真 <15231627695@163.com>
2021-04-06 16:33:02 +0800

    feat：登录

---
+++ src/views/Home/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import './index.less';
+import { Button } from 'antd';
+
+const Home: FC = () => (
+  <div className="Home_views">
+    <Button type="primary">Home</Button>
+  </div>
+);
+
+export default Home;
---
+++ src/views/Login/index.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from 'react';
+import './index.less';
+import { Form, Input, Button, Card } from 'antd';
+
+type IProps = {
+    isLogin: boolean
+}
+const Login: FC<IProps> = (props) => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+    console.log(this,"this")
+    console.log(props,"props")
+  };
+  const onFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+  return (
+    <div className="Login_views">
+      <Card title="登录页面" style={{ width: 500, minHeight: '300px', boxShadow:'1px 3px 8px 0 rgb(30 112 72 / 35%)' }}>
+        <Form
+          name="basic"
+          onFinish={onFinish}
+          onFinishFailed={onFailed}
+        >
+          <Form.Item
+            label=""
+            name="username"
+            rules={[{ required: true, message: '请输入用户名!' }]}
+          >
+            <Input allowClear/>
+          </Form.Item>
+
+          <Form.Item
+            label=""
+            name="password"
+            rules={[{ required: true, message: '请输入密码!' }]}
+          >
+            <Input.Password allowClear/>
+          </Form.Item>
+          <Button type="primary" block htmlType="submit">登  录</Button>
+        </Form>
+      </Card>
+    </div>
+  )
+}
+
+export default Login;
